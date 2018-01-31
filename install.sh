5c0e7b0d22c1487487c6b54662a6cac12cd90634
Nikolay Dikhtyar <nikolay.dikhtyar@onix-systems.com>
2018-01-31 11:14:34 +0200

    Changed basic docker image to use for retrieving certificates

--- install.sh
+++ install.sh
@@ -167,7 +167,7 @@ else
             -v "$(pwd)/data/letsencrypt/etc:/etc/letsencrypt" \
             -v "$(pwd)/data/letsencrypt/var:/var/lib/letsencrypt" \
             ${OPTION} \
-            quay.io/letsencrypt/letsencrypt \
+            certbot/certbot \
             ${CERTBOT_OPTIONS}
     fi
     echo "Adding cron task for reloading service, that uses this ceritificate"
@@ -178,6 +178,6 @@ cat << EOF > ${CROND_FOLDER}/${CRON_TASK}
 SHELL=/bin/bash
 # Command that will help to renew and reload service for using new certificate
 # ${DN}
-${CRON_TASK_SCHEDULE} root docker run --rm -v "$(pwd)/data/letsencrypt/etc:/etc/letsencrypt" -v "$(pwd)/data/letsencrypt/var:/var/lib/letsencrypt" quay.io/letsencrypt/letsencrypt renew ${COMMAND}
+${CRON_TASK_SCHEDULE} root docker run --rm -v "$(pwd)/data/letsencrypt/etc:/etc/letsencrypt" -v "$(pwd)/data/letsencrypt/var:/var/lib/letsencrypt" certbot/certbot renew ${COMMAND}
 EOF
 fi
